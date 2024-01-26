bf2234ed84268cc50210803cdb65bbcfae84b564
Vlad Ilie <vilie@adobe.com>
2024-01-26 11:11:24 +0200

    chore: change datastream id and org id

--- scripts/analytics/lib-analytics.js
+++ scripts/analytics/lib-analytics.js
@@ -51,8 +51,8 @@ function getAlloyInitScript() {
 function getDatastreamConfiguration() {
   // Sites Internal
   return {
-    edgeConfigId: 'caad777c-c410-4ceb-8b36-167f1cecc3de',
-    orgId: '908936ED5D35CC220A495CD4@AdobeOrg',
+    edgeConfigId: 'caf850a0-755c-4164-974f-87d15f3864ac',
+    orgId: '4E9432245BC7C44B0A494037@AdobeOrg',
   };
 }
 
