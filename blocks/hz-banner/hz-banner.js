c6b765517cba5642535ff00e52a9b82df433533b
ilievlad73 <ilievlad73@gmail.com>
2024-07-08 15:00:18 +0300

    default hz banner block

--- blocks/hz-banner/hz-banner.js
+++ blocks/hz-banner/hz-banner.js
@@ -1,11 +1,5 @@
 import html from './hz-banner-html.js';
 
 export default function decorate(block) {
-  let hydratedHTML = html;
-  [...block.children].forEach((row, i) => {
-    [...row.children].forEach((rowChild, j) => {
-      hydratedHTML = hydratedHTML.replace(`{{${j}}}`, rowChild.innerText.trim());
-    });
-  });
-  block.innerHTML = hydratedHTML; 
+  block.innerHTML = html; 
 };
